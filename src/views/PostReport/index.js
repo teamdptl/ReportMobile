14e67989f4f7693d061a523e3df53e349e0a958d
trangthanhphuc <trangthanhphuc91@gmail.com>
2023-10-16 16:23:50 +0700

    update 1 phần giao diện create report

--- src/views/PostReport/index.js
+++ src/views/PostReport/index.js
@@ -1,22 +1,27 @@
-import { Text, View, TextInput } from 'react-native'
+import { Text, View, TextInput, Image } from 'react-native'
 import React from 'react'
 import styles from './style';
 
 
 const PostReport = ({navigation}) => {
   return (
-    <View>
-              <Text>Phản hồi thông tin</Text>
-              <Text>Tiêu đề</Text>
-              <TextInput />
-              <Text>Mô tả báo cáo</Text>
-              <TextInput />
-              <Text>Địa điểm</Text>
-              <TextInput />
-              <Text>Thêm ảnh</Text>
-              
+    <View style={styles.container}>               
+     <Image source={require('../../assets/images/return.png')} style={{width: 50, height: 50}} />
 
-    </View>
+      <View style={styles.textHeader}>
+        <Text style={styles.centeredText}>Phản hồi thông tin</Text>
+      </View>
+      <View style={styles.bodyReport}>
+          <Text style={styles.textBody}>Tiêu đề</Text>
+          <TextInput style={styles.roundedInput} />
+          <Text style={styles.textBody}>Mô tả báo cáo</Text>
+          <TextInput style={[styles.roundedInput, styles.paraInput]} multiline={true} />
+          <Text style={styles.textBody}>Địa điểm</Text>
+          <TextInput style={styles.roundedInput} />
+          <Text style={styles.textBody}>Thêm ảnh</Text>
+      </View>
+    
+  </View>
       
   )
 }
--- src/views/UserReport/index.js
+++ src/views/UserReport/index.js
@@ -1,12 +1,21 @@
-import React, { useRef, useState } from 'react';
-import { StyleSheet, Text, View, StatusBar, ScrollView, Animated, FlatList, Dimensions } from 'react-native';
-import styles from './style';
-import HeaderComponent from '../../components/HeaderComponent'; // Import your new component
-import SmallButton from '../../components/SmallButtons';
-import color from '../../contains/color';
-import Body from '../../components/Body';
-import Task from '../../components/Task';
-const { width: WINDOW_WIDTH, height: WINDOW_HEIGHT } = Dimensions.get('window');
+import React, { useRef, useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  StatusBar,
+  ScrollView,
+  Animated,
+  FlatList,
+  Dimensions,
+} from "react-native";
+import styles from "./style";
+import HeaderComponent from "../../components/HeaderComponent"; // Import your new component
+import SmallButton from "../../components/SmallButtons";
+import color from "../../contains/color";
+import Body from "../../components/Body";
+import Task from "../../components/Task";
+const { width: WINDOW_WIDTH, height: WINDOW_HEIGHT } = Dimensions.get("window");
 
 const UserReport = ({ navigation }) => {
   const animatedValue = useRef(new Animated.Value(0)).current;
@@ -14,37 +23,29 @@ const UserReport = ({ navigation }) => {
   const [addComponentAsLongPress, setaddComponentAsLongPress] = useState(false);
 
   const [taskList, setTaskList] = useState([
-    { key: 'Task 1' },
-    { key: 'Task 2' },
-    { key: 'Task 3' },
-    { key: 'Task 4' },
-    { key: 'Task 5' },
+    { key: "Task 1" },
+    { key: "Task 2" },
+    { key: "Task 3" },
+    { key: "Task 4" },
+    { key: "Task 5" },
 
     // Thêm các mục khác vào đây
   ]);
 
-
   const handleLongPress = () => {
     // Thiết lập một hẹn giờ để kiểm tra liệu người dùng có giữ trong ít nhất 2 giây không
     setTimeout(() => {
-        setLongPress(true);
-        setaddComponentAsLongPress(true);
+      setLongPress(true);
+      setaddComponentAsLongPress(true);
     }, 1000);
-}
-  
+  };
 
-
-  
-
-
-
-  const contentHeight = taskList.length >= 7 ? {} : { height: WINDOW_HEIGHT * 1 };
-
-
+  const contentHeight =
+    taskList.length >= 7 ? {} : { height: WINDOW_HEIGHT * 1 };
 
   return (
     <View style={styles.container}>
-      <StatusBar barStyle='light-content' backgroundColor="#0693F1" />
+      <StatusBar barStyle="light-content" backgroundColor="#0693F1" />
 
       <View style={styles.upperHeader}>
         {/* Render anything you want for the upper header here */}
@@ -61,38 +62,47 @@ const UserReport = ({ navigation }) => {
         scrollEventThrottle={16}
       >
         <View style={styles.paddingForHeader} />
-        <View style={[styles.scrollViewContent, contentHeight ]}>
+        <View style={[styles.scrollViewContent, contentHeight]}>
           <View style={styles.containerLine}>
             <View style={styles.line}></View>
           </View>
 
           <View style={styles.containerHTZLMid}>
             <View style={styles.leftContent}>
-              <Text style={{ fontWeight: 'bold' }}>Danh sách phản hồi</Text>
+              <Text style={{ fontWeight: "bold" }}>Danh sách phản hồi</Text>
             </View>
-            {
-             addComponentAsLongPress ?  ( <View style={styles.rightContent}>
-                <SmallButton title="Trở về" buttonColor={color.primaryColor} onPress={() => { setLongPress(false),setaddComponentAsLongPress(false)  }} />
-                <SmallButton title="Xóa" buttonColor={color.red} onPress={() => {  }}  />
-              </View> ) : null
-            }
-          
+            {addComponentAsLongPress ? (
+              <View style={styles.rightContent}>
+                <SmallButton
+                  title="Trở về"
+                  buttonColor={color.primaryColor}
+                  onPress={() => {
+                    setLongPress(false), setaddComponentAsLongPress(false);
+                  }}
+                />
+                <SmallButton
+                  title="Xóa"
+                  buttonColor={color.red}
+                  onPress={() => {}}
+                />
+              </View>
+            ) : null}
           </View>
 
-          <View style={{marginTop:10}}>
-               {
-                   taskList.map((item,index)=>{
-
-                       return <Task key={index} index={index} handleLongPress={handleLongPress} longPress={longPress}  />;
-                   })
-               }
-           </View>
-          
-          
+          <View style={{ marginTop: 10 }}>
+            {taskList.map((item, index) => {
+              return (
+                <Task
+                  key={index}
+                  index={index}
+                  handleLongPress={handleLongPress}
+                  longPress={longPress}
+                />
+              );
+            })}
+          </View>
         </View>
       </Animated.ScrollView>
-
-     
     </View>
   );
 };
