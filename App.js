a8ac2046a3f586ea1eef95ad2f8b7a308946f298
huykhaduy <huykhaduy@gmail.com>
2023-10-21 23:02:30 +0700

    Update App.js

--- App.js
+++ App.js
@@ -1,39 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { SafeAreaView } from 'react-native';
 import GlobalStyles from './src/components/GlobalStyles';
 
-
+// Chứa các screen của ứng dụng
 import NavigationContainerWrapper from './src/navigation/NavigationContainerWrapper';
-
-import Splash from './src/views/Splash';
-import Login from './src/views/Login';
-import UserReport from './src/views/UserReport';
-
-const Stack = createNativeStackNavigator();
-import TabScreenUser from './src/navigation/TabScreenUser';
 
 export default function App() {
   return (
       <SafeAreaView style={GlobalStyles.androidSafeArea}>
-        {/* <NavigationContainer>
-        <Stack.Navigator screenOptions={{headerShown:false}}>
-          <Stack.Screen name='Splash' component={Splash} />
-          <Stack.Screen name='Login' component={Login} />
-          <Stack.Screen name='UserReport' component={UserReport} />
-          
-        </Stack.Navigator>
-      </NavigationContainer> */}
-
         <NavigationContainerWrapper />
-      {/* <TabScreenUser /> */}
-
       </SafeAreaView>
-      
   );
 }
-
-const styles = StyleSheet.create({
-  
-});
